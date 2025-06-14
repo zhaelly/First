796689e3e3ab637e2c2b048d5f3c9e2c33920284
zhaelly <zitzitbags@gmail.com>
2025-06-14 20:03:43 +0800

    Update Music.py

--- pages/Music.py
+++ pages/Music.py
@@ -12,3 +12,10 @@ with st.container():
     with col3:
         st.subheader("🎵 Stream")
         st.markdown('[**"AGAIN" by zhaelly**](https://push.fm/fl/Z2572Hrc)')
+   
+    col1, col2, col3 = st.columns([1, 2, 5])
+    with col2:
+        st.image("images/spotify logo.png", use_container_width=True)
+    with col3:
+        st.subheader("🎵 Stream")
+        st.markdown('[**"wish" by zhaelly**](https://push.fm/fl/yyk8rzj2)')
