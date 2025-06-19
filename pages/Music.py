6bd4579b2db40e71bd1e722b5fb7fc756764a0d2
zhaelly <zitzitbags@gmail.com>
2025-06-19 11:14:51 +0800

    Update Music.py

--- pages/Music.py
+++ pages/Music.py
@@ -8,14 +8,16 @@ with st.container():
     # First Example: Spotify release
     col1, col2, col3 = st.columns([1, 2, 5])
     with col2:
-        st.image("images/spotify logo.png", use_container_width=True)
+        st.image("images/zhaelly-wish.jpg", use_container_width=True)
+    with col3:
+        st.subheader("🎵 Stream")
+        st.markdown('[**"wish" by zhaelly**](https://push.fm/fl/yyk8rzj2)')
+        
+    col1, col2, col3 = st.columns([1, 2, 5])
+    with col2:
+        st.image("images/zhaelly-AGAIN.jpg", use_container_width=True)
     with col3:
         st.subheader("🎵 Stream")
         st.markdown('[**"AGAIN" by zhaelly**](https://push.fm/fl/Z2572Hrc)')
    
-    col1, col2, col3 = st.columns([1, 2, 5])
-    with col2:
-        st.image("images/spotify logo.png", use_container_width=True)
-    with col3:
-        st.subheader("🎵 Stream")
-        st.markdown('[**"wish" by zhaelly**](https://push.fm/fl/yyk8rzj2)')
+    
