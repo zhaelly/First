fa93dcb584449968e71f15147a60967e6a09c7d1
zhaelly <zitzitbags@gmail.com>
2025-07-04 10:23:47 +0800

    Update About_Me.py

--- pages/About_Me.py
+++ pages/About_Me.py
@@ -12,9 +12,9 @@ with col2:
         st.write("###")
         st.write("🎹 I’ve been playing piano for over 8 years, rocking the drums for 5 years, and guitar for 4 years. With over 2 years of music production under my belt, I focus mainly on EDM, especially progressive house and pop house, though I love to experiment with rock and other genres.")
         st.write("###")
-        st.write("You can find me on Spotify, TikTok (@zhaelly.zhae), and raket.ph, where I offer custom songwriting services and sell digital products like stickers, templates, and art.")
+        st.write("You can find me on Spotify and TikTok (@zhaelly.zhae).")
         st.write("###")
-        st.write("When I’m not making music, I’m wrapping up Grade 11 and getting ready for my future. I plan to study Computer Science in college and am saving up for a new laptop to level up my production and workflow. I enjoy capturing moments with my Canon IXY camera and hope to build a portfolio for my future career.")
+        st.write("When I’m not making music, I’m wrapping up Grade 12 and getting ready for my future. I plan to study Computer Science in college and am saving up for a new laptop to level up my production and workflow. I enjoy capturing moments and hope to build a portfolio for my future career.")
         st.write("###")
         st.write("Thanks for visiting my page! If you want to collaborate or work on a project together, feel free to reach out! Let’s create something great! 🚀")
 
